a21f8030d8113297bf57c149014367eb175edeb8
ardin <ardin@ARDIN-PC>
2023-08-27 13:08:40 +0330

    first commit

---
+++ Ardin.MongodbGenericRepository/Ardin.MongodbGenericRepository/IDocument.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Ardin.MongodbGenericRepository
+{
+    public interface IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        string _id { get; set; } 
+        DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Ardin.MongodbGenericRepository/Ardin.MongodbGenericRepository/MongoRepository.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace Ardin.MongodbGenericRepository
+{
+    public class MongoRepository<TDocument> : IMongoRepository<TDocument>
+    where TDocument : IDocument
+    {
+        private readonly IMongoCollection<TDocument> _collection;
+
+        public MongoRepository(IConfiguration configuration)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"));
+            var root = builder.Build();
+            var mongoConnectionString = root.GetSection("MongoConnectionString").Value;
+            var mongoDatabase = root.GetSection("MongoDatabase").Value;
+            var database = new MongoClient(mongoConnectionString).GetDatabase(mongoDatabase);
+            _collection = database.GetCollection<TDocument>(GetCollectionName(typeof(TDocument)));
+        }
+
+        private protected string GetCollectionName(Type documentType)
+        {
+            return ((BsonCollectionAttribute)documentType.GetCustomAttributes(
+                    typeof(BsonCollectionAttribute),
+                    true)
+                .FirstOrDefault())?.CollectionName;
+        }
+
+        public virtual IQueryable<TDocument> AsQueryable()
+        {
+            return _collection.AsQueryable();
+        }
+
+        public virtual IEnumerable<TDocument> FilterBy(
+            Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return _collection.Find(filterExpression).ToEnumerable();
+        }
+
+        public virtual IEnumerable<TProjected> FilterBy<TProjected>(
+            Expression<Func<TDocument, bool>> filterExpression,
+            Expression<Func<TDocument, TProjected>> projectionExpression)
+        {
+            return _collection.Find(filterExpression).Project(projectionExpression).ToEnumerable();
+        }
+
+        public virtual TDocument FindOne(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return _collection.Find(filterExpression).FirstOrDefault();
+        }
+
+        public virtual Task<TDocument> FindOneAsync(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return Task.Run(() => _collection.Find(filterExpression).FirstOrDefaultAsync());
+        }
+
+        public virtual TDocument FindById(string id)
+        {
+            var filter = Builders<TDocument>.Filter.Eq(doc => doc._id, id);
+            return _collection.Find(filter).SingleOrDefault();
+        }
+
+        public virtual Task<TDocument> FindByIdAsync(string id)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<TDocument>.Filter.Eq(doc => doc._id, id);
+                return _collection.Find(filter).SingleOrDefaultAsync();
+            });
+        }
+
+
+        public virtual void InsertOne(TDocument document)
+        {
+            if (document._id == ObjectId.Empty.ToString())
+            {
+                document._id = ObjectId.GenerateNewId().ToString();
+                document.CreatedAt = DateTime.Now;
+            }
+            _collection.InsertOne(document);
+        }
+
+        public virtual Task InsertOneAsync(TDocument document)
+        {
+            return Task.Run(() => _collection.InsertOneAsync(document));
+        }
+
+        public void InsertMany(ICollection<TDocument> documents)
+        {
+            _collection.InsertMany(documents);
+        }
+
+
+        public virtual async Task InsertManyAsync(ICollection<TDocument> documents)
+        {
+            await _collection.InsertManyAsync(documents);
+        }
+
+        public void ReplaceOne(TDocument document)
+        {
+            var filter = Builders<TDocument>.Filter.Eq(doc => doc._id, document._id);
+            _collection.FindOneAndReplace(filter, document);
+        }
+
+        public virtual async Task ReplaceOneAsync(TDocument document)
+        {
+            var filter = Builders<TDocument>.Filter.Eq(doc => doc._id, document._id);
+            await _collection.FindOneAndReplaceAsync(filter, document);
+        }
+
+        public void DeleteOne(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            _collection.FindOneAndDelete(filterExpression);
+        }
+
+        public Task DeleteOneAsync(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return Task.Run(() => _collection.FindOneAndDeleteAsync(filterExpression));
+        }
+
+        public void DeleteById(string id)
+        {
+            var filter = Builders<TDocument>.Filter.Eq(doc => doc._id, id);
+            _collection.FindOneAndDelete(filter);
+        }
+
+        public Task DeleteByIdAsync(string id)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<TDocument>.Filter.Eq(doc => doc._id, id);
+                _collection.FindOneAndDeleteAsync(filter);
+            });
+        }
+
+        public void DeleteMany(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            _collection.DeleteMany(filterExpression);
+        }
+
+        public Task DeleteManyAsync(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return Task.Run(() => _collection.DeleteManyAsync(filterExpression));
+        }
+    }
+}
